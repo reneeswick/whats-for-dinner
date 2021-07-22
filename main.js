271587efa9d151ddd2975d8b02640ebd243092f2
Renee Swick-Ziller <reneeswick@gmail.com>
2021-07-22 14:08:24 -0600

    Refactor clear button

--- main.js
+++ main.js
@@ -58,11 +58,48 @@ var formOptionSide = document.querySelector('#side');
 var formOptionMain = document.querySelector('#main-dish');
 var formOptionDessert = document.querySelector('#dessert');
 var formOptionEntireMeal = document.querySelector('#entire-meal');
+var clearBtn = document.querySelector('.clear');
 
 // event listeners
-letsCookButton.addEventListener('click', generateDish);
+// letsCookButton.addEventListener('click', generateDish);
 //
-// // event handlers
+// function generateDish(){
+//   event.preventDefault();
+//
+//   var sideChecked = formOptionSide.checked;
+//   var mainChecked = formOptionMain.checked;
+//   var dessertChecked = formOptionDessert.checked;
+//   var entireMealChecked = formOptionEntireMeal.checked;
+//
+//   var randomSide = sides[Math.floor(Math.random() * sides.length)];
+//   var randomMain = mains[Math.floor(Math.random() * mains.length)];
+//   var randomDessert = desserts[Math.floor(Math.random() * desserts.length)];
+//
+//    if(sideChecked === true){
+//     youShouldMake.innerHTML =
+//     `<h5 class="message">You should make:</h5>
+//      <h1 class="random-dish"> ${randomSide}!</h1>
+//      <button class="clear">Clear</button>`;
+//     }else if (mainChecked === true){
+//       youShouldMake.innerHTML =
+//       `<h5 class="message">You should make:</h5>
+//        <h1 class="random-dish"> ${randomMain}!</h1>
+//        <button class="clear">Clear</button>`;
+//     }else if(dessertChecked === true){
+//       youShouldMake.innerHTML =
+//       `<h5 class="message">You should make:</h5>
+//        <h1 class="random-dish"> ${randomDessert}!</h1>
+//        <button class="clear">Clear</button>`;
+//     }else if(entireMealChecked === true){
+//       youShouldMake.innerHTML =
+//       `<h5 class="message">You should make:</h5>
+//        <h1 class="random-dish"> ${randomSide} with a side of ${randomMain} and ${randomDessert} for dessert!</h1>
+//        <button class="clear">Clear</button>`;
+//     }
+//   };
+
+letsCookButton.addEventListener('click', generateDish);
+
 function generateDish(){
   event.preventDefault();
 
@@ -78,22 +115,22 @@ function generateDish(){
    if(sideChecked === true){
     youShouldMake.innerHTML =
     `<h5 class="message">You should make:</h5>
-     <h1 class="random-dish"> ${randomSide}!</h1>
-     <button class="clear">Clear</button>`;
+     <h1 class="random-dish"> ${randomSide}!</h1>`;
+    clearBtn.classList.remove('hidden');
     }else if (mainChecked === true){
       youShouldMake.innerHTML =
       `<h5 class="message">You should make:</h5>
-       <h1 class="random-dish"> ${randomMain}!</h1>
-       <button class="clear">Clear</button>`;
+       <h1 class="random-dish"> ${randomMain}!</h1>`;
+      clearBtn.classList.remove('hidden');
     }else if(dessertChecked === true){
       youShouldMake.innerHTML =
       `<h5 class="message">You should make:</h5>
-       <h1 class="random-dish"> ${randomDessert}!</h1>
-       <button class="clear">Clear</button>`;
+       <h1 class="random-dish"> ${randomDessert}!</h1>`;
+      clearBtn.classList.remove('hidden');
     }else if(entireMealChecked === true){
       youShouldMake.innerHTML =
       `<h5 class="message">You should make:</h5>
-       <h1 class="random-dish"> ${randomSide}<br>${randomMain}<br>${randomDessert}!</h1>
-       <button class="clear">Clear</button>`;
+       <h1 class="random-dish"> ${randomSide} with a side of ${randomMain} and ${randomDessert} for dessert!</h1>`;
+      clearBtn.classList.remove('hidden');
     }
   };
